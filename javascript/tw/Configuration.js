a1e56cebffd3d4f4235c489020d1c0a574032a66
cody.wu <cody.wu@mail.emis.com.tw>
2021-07-06 18:28:23 +0800

    add tablesort.css

--- javascript/tw/Configuration.js
+++ javascript/tw/Configuration.js
@@ -25,7 +25,7 @@
 (function(root) {
 
 	var result;
-
+	
 	if (typeof process !== 'undefined') {
 
 		// nodeJS執行環境
@@ -79,8 +79,55 @@
 	
 	if (typeof result === 'undefined') throw new Error('DefaultConfiguration is undefined!');
 
+	/**
+	 *
+	 * @description window.onload
+	 *
+	 * @see {@link https://api.jquery.com/ready/|.ready() | jQuery API Documentation}
+	 *
+	 * @see {@link https://blog.miniasp.com/post/2007/11/24/14-rules-for-faster-front-end-performance-notes|The Will Will Web | 加速前端網頁效能的14條規則}
+	 * @see {@link https://ithelp.ithome.com.tw/articles/10092601|jQuery 筆記 (四) window.onload 與 $(document).ready - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天}
+	 * @see {@link https://blog.miniasp.com/post/2010/07/24/jQuery-ready-vs-load-vs-window-onload-event|The Will Will Web | 使用 jQuery(document).ready() 與 window.onload 注意事項}
+	 *
+	 * @comment 只要在註冊 window.onload 事件後才使用 $(document).ready() 就會導致 $(document).ready() 變的與 $(window).load() 的行為一樣，要等到網頁所有資源都下載完畢才會執行 $(document).ready() 中註冊的事件！
+	 *
+	 */
+	// All resources finished loading!
+	// window.addEventListener('load', function(event) {});
+	
+	/**
+	 *
+	 * @description DOMContentLoaded
+	 *
+	 * @see {@link https://developer.mozilla.org/zh-TW/docs/Web/Events/DOMContentLoaded|DOMContentLoaded - Event reference | MDN}
+	 *
+	 * @see {@link https://api.jquery.com/ready/|.ready() | jQuery API Documentation}
+	 *
+	 * @see {@link https://wowtianwen.iteye.com/blog/2100913|原生JS实现document.ready - - ITeye博客}
+	 * @see {@link https://ithelp.ithome.com.tw/articles/10197335|重新認識 JavaScript 番外篇 (6) - 網頁的生命週期 - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天}
+	 *
+	 */
+	if ((typeof Packages === 'undefined') && (typeof document !== 'undefined')) {
+	
+		document.addEventListener('DOMContentLoaded', function() {
+		
+			// nwJS的location.protocol也是定義成chrome-extension:。
+			if ((result.location.protocol == 'chrome-extension:') || (result.location.protocol == 'file:')) {
+			
+				// NW.js由inject_js_end屬性載入執行。
+				if (typeof nw === 'undefined') result.loadJS(location.pathname.substring(1, location.pathname.lastIndexOf('/') + 1) + 'nw_inject_end.js');
+			}
+			else if ((result.location.protocol == 'http:') || (result.location.protocol == 'https:')) {
+				
+				// if (result.loadNWInjectEnd() === 'Y') result.loadJS('nw_inject_end.js');
+				// if (result.loadNWInjectEnd() === 'Y') result.loadJS(location.pathname.substring(1, location.pathname.lastIndexOf('/') + 1) + 'nw_inject_end.js');
+				if (result.loadNWInjectEnd() === 'Y') result.loadJS(location.origin + location.pathname.substring(0, location.pathname.lastIndexOf('/') + 1) + 'nw_inject_end.js');
+			}
+		});
+	}
+
 	// Browser environment
-	result["JSLibDir"] = 'javascript';
+	// result["JSLibDir"] = 'javascript';
 	result["DAODir"] = 'tw/ace33022/dao/browser';
 	result["BrowserUIDir"] = 'tw/ace33022/program/browser/bootstrap';
 	
@@ -99,7 +146,7 @@
 		
 			if ((result.location.origin.indexOf('127.0.0.1') === -1) && (result.location.origin.indexOf('localhost') === -1)) {
 			
-				result["JSLibDir"] = 'https://ace33022.github.io';
+				// result["JSLibDir"] = 'https://ace33022.github.io/htdoc/javascript';
 
 				// result["requirejsFile"] = 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.2.0/require.js';
 				result["requirejsFile"] = 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.2.0/require';
@@ -107,6 +154,7 @@
 				result["paths"]["underscore"] = 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min';
 				result["paths"]["backbone"] = 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min';
 				result["paths"]["tablesort"] = 'https://cdnjs.cloudflare.com/ajax/libs/tablesort/5.0.2/tablesort.min';
+				result["paths"]["tablesort.number"] = 'https://cdnjs.cloudflare.com/ajax/libs/tablesort/5.0.2/sorts/tablesort.number.min';
 				
 				result["paths"]["jquery"] = 'https://code.jquery.com/jquery-1.12.3.min';
 				result["paths"]["bootstrap"] = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min';
@@ -204,53 +252,6 @@
 		}
 	}
 		
-	/**
-	 *
-	 * @description window.onload
-	 *
-	 * @see {@link https://api.jquery.com/ready/|.ready() | jQuery API Documentation}
-	 *
-	 * @see {@link https://blog.miniasp.com/post/2007/11/24/14-rules-for-faster-front-end-performance-notes|The Will Will Web | 加速前端網頁效能的14條規則}
-	 * @see {@link https://ithelp.ithome.com.tw/articles/10092601|jQuery 筆記 (四) window.onload 與 $(document).ready - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天}
-	 * @see {@link https://blog.miniasp.com/post/2010/07/24/jQuery-ready-vs-load-vs-window-onload-event|The Will Will Web | 使用 jQuery(document).ready() 與 window.onload 注意事項}
-	 *
-	 * @comment 只要在註冊 window.onload 事件後才使用 $(document).ready() 就會導致 $(document).ready() 變的與 $(window).load() 的行為一樣，要等到網頁所有資源都下載完畢才會執行 $(document).ready() 中註冊的事件！
-	 *
-	 */
-	// All resources finished loading!
-	// window.addEventListener('load', function(event) {});
-	
-	/**
-	 *
-	 * @description DOMContentLoaded
-	 *
-	 * @see {@link https://developer.mozilla.org/zh-TW/docs/Web/Events/DOMContentLoaded|DOMContentLoaded - Event reference | MDN}
-	 *
-	 * @see {@link https://api.jquery.com/ready/|.ready() | jQuery API Documentation}
-	 *
-	 * @see {@link https://wowtianwen.iteye.com/blog/2100913|原生JS实现document.ready - - ITeye博客}
-	 * @see {@link https://ithelp.ithome.com.tw/articles/10197335|重新認識 JavaScript 番外篇 (6) - 網頁的生命週期 - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天}
-	 *
-	 */
-	if (typeof document !== 'undefined') {
-	
-		document.addEventListener('DOMContentLoaded', function() {
-		
-			// nwJS的location.protocol也是定義成chrome-extension:。
-			if ((result.location.protocol == 'chrome-extension:') || (result.location.protocol == 'file:')) {
-			
-				// NW.js由inject_js_end屬性載入執行。
-				if (typeof nw === 'undefined') result.loadJS(location.pathname.substring(1, location.pathname.lastIndexOf('/') + 1) + 'nw_inject_end.js');
-			}
-			else if ((result.location.protocol == 'http:') || (result.location.protocol == 'https:')) {
-				
-				// if (result.loadNWInjectEnd() === 'Y') result.loadJS('nw_inject_end.js');
-				// if (result.loadNWInjectEnd() === 'Y') result.loadJS(location.pathname.substring(1, location.pathname.lastIndexOf('/') + 1) + 'nw_inject_end.js');
-				if (result.loadNWInjectEnd() === 'Y') result.loadJS(location.origin + location.pathname.substring(0, location.pathname.lastIndexOf('/') + 1) + 'nw_inject_end.js');
-			}
-		});
-	}
-
 	if (typeof process !== 'undefined') {
 	
 		// nodeJS執行環境
